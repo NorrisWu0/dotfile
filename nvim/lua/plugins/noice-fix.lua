4d6885474569e7128de610b839d80678f1073cc3
Norris Wu <me@norriswu.me>
2026-07-30 23:14:50 +1000

    feat(nvim): sync from live omarchy install
    add remote clipboard for tmux/ssh, improve theme hot-reload for
    aether.nvim, refactor transparency to loop, add 6 new themes,
    fix theme plugin repos, drop stale neo-tree/noice/tailwind plugins

--- nvim/lua/plugins/noice-fix.lua
+++
@@ -1,5 +0,0 @@
--- Disable noice.nvim due to treesitter query compatibility issue
--- https://github.com/nvim-treesitter/nvim-treesitter/issues/8369
-return {
-  { "folke/noice.nvim", enabled = false },
-}
