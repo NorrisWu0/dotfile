69eb198f4c5f702c527d00fe0b5cea2e670a954d
norriswu0 <me@norriswu.me>
2026-09-13 07:16:50 +0000

    feat(nvim): replace markdown preview plugin

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,16 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = "cd app && npx --yes yarn install",
+  "selimacerbas/markdown-preview.nvim",
+  dependencies = { "selimacerbas/live-server.nvim" },
+  cmd = { "MarkdownPreview", "MarkdownPreviewRefresh", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  opts = {
+    host = "0.0.0.0",
+    open_browser = false,
+    hooks = {
+      on_start = function(url)
+        local devbox_url = url:gsub("^(https?://)[^:/]+", "%1devbox")
+        vim.notify(devbox_url, vim.log.levels.INFO)
+      end,
+    },
+  },
 }
