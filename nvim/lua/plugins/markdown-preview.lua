5a5cef998968d097c8b5ae945c43348e2f18aa62
norriswu0 <me@norriswu.me>
2026-09-26 07:12:27 +0000

    feat(nvim): improve markdown preview workflow

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -3,13 +3,24 @@ return {
   dependencies = { "selimacerbas/live-server.nvim" },
   cmd = { "MarkdownPreview", "MarkdownPreviewRefresh", "MarkdownPreviewStop" },
   ft = { "markdown" },
+  keys = {
+    {
+      "<leader>mr",
+      "<cmd>MarkdownPreview<cr>",
+      desc = "Toggle Markdown Preview"
+    }
+  },
   opts = {
     host = "0.0.0.0",
     open_browser = false,
     hooks = {
       on_start = function(url)
         local devbox_url = url:gsub("^(https?://)[^:/]+", "%1devbox")
-        vim.notify(devbox_url, vim.log.levels.INFO)
+        vim.fn.setreg("+", devbox_url)
+        vim.fn.setreg('"', devbox_url)
+        vim.notify("Markdown preview URL copied to clipboard", vim.log.levels.INFO, {
+          timeout = 8000,
+        })
       end,
     },
   },
